6dae8cc99ef097085392f9ef5beaa3206d6ead97
Shubham Rawat <shubham25112000@gmail.com>
2021-10-19 01:04:00 +0530

    Implemented Debouncing

--- food_api_1/recipeOfTheDay.js
+++ food_api_1/recipeOfTheDay.js
@@ -1,8 +1,9 @@
-import navBarFunction from "./navbar.js";
+import navBarFunction  from "./navbar.js";
 
 var nav = document.getElementsByClassName("navigation")[0];
-var navbar = navBarFunction();
-nav.innerHTML = navbar;
+nav.innerHTML = navBarFunction();
+
+var dishes = document.getElementById("dishes");
 
 var div1 = document.getElementById('1');
 var div2 = document.getElementById('2');
@@ -21,12 +22,9 @@ div3.addEventListener( "click", () => {
 })
 
 
-var btn = document.getElementById("submit");
-var receipe = document.getElementById("receipe");
-var dishes = document.getElementById("dishes");
 
 function createCard( mealData ){
-    console.log( mealData );
+    mealData = mealData[0];
 
     var cuisine = mealData.strArea;
     console.log( cuisine );
@@ -39,14 +37,10 @@ function createCard( mealData ){
     var receipe = mealData.strInstructions;
     console.log( receipe );
 
-    for( var i = 1; i <= 20; i++ ){
-        if( mealData[`strIngredient${i}`] )
-            console.log( mealData[`strIngredient${i}`], mealData[`strMeasure${i}`] );
-    }
-
+    
     var mealName = mealData.strMeal;
     console.log( mealName );
-
+    
     var div = document.createElement( 'div' );
     var h1 = document.createElement( 'h1' );
     h1.textContent = mealName;
@@ -54,6 +48,18 @@ function createCard( mealData ){
     var img = document.createElement( 'img' );
     img.src = image;
     div.appendChild( img );
+
+    for( var i = 1; i <= 20; i++ ){
+        var ingredient;
+        if( mealData[`strIngredient${i}`] ){
+
+            ingredient = mealData[`strIngredient${i}`] + " - " + mealData[`strMeasure${i}`];
+            var span = document.createElement("span");
+            span.textContent = ingredient;
+            div.appendChild( span );
+
+        }
+    }
     var p = document.createElement( 'p' );
     p.textContent = receipe;
     div.appendChild(p);
@@ -61,43 +67,43 @@ function createCard( mealData ){
     dishes.appendChild(div);
 }
 
-function getReceipe(){
-    var value = receipe.value;
-    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`;
-
-    console.log( "URL+", url );
-
-
+function getRecipe(){
+    dishes.innerHTML = "";
+    var url = `https://www.themealdb.com/api/json/v1/1/random.php`;
     fetch( url )
     .then( res => {
         return res.json();
     })
     .then( data => {
-        var allMeals = data.meals;
-        dishes.innerHTML = "";
-        for( var i = 0; i < allMeals.length; i++ ){
-            createCard( allMeals[i] );
-        }
+        var meal = data.meals;
+        createCard( meal );
     })
     .catch( err => {
         console.log( err );
     })
 }
 
-const debounce = (func, timeout) => {
+var debounce = (func, timeout = 1000*24*60*60 ) => {
     var timer;
     return (...args) => {
-
-        clearTimeout( timer );
-
-        timer = setTimeout( () => {
-            func.apply(this, args);
-        }, timeout );
-
+        if( timer )
+            clearTimeout( timer );
+        timer = setTimeout(() => {
+            func.apply( this, args );
+        }, timeout);
     }
 }
 
-receipe.addEventListener("input", debounce( () => {
-    getReceipe();
-}, 500));
+window.addEventListener("load", () => {
+    console.log( dishes.innerHTML === "" );
+    console.log( dishes.innerHTML );
+    if( dishes.innerHTML === "" ){
+        getRecipe();
+    }
+    else{
+        debounce( () => {
+            getRecipe();
+        }, 2000)
+    }
+});
 
